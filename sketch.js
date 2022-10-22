e590a44bf05bf13e8a2426ef425cb930a3b80bfd
Alexis Gorby <acg20br@fsu.edu>
2022-10-21 22:08:38 -0400

    made basic animation

--- sketch.js
+++ sketch.js
@@ -1,7 +1,57 @@
+let x=0
+let y= 0
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(125);
+  background(200);
+
+  stroke(50);
+  fill("red");
+  circle(60, y, 60, 30);
+
+  stroke(50);
+  fill("pink");
+  circle(90, y, 60, 30);
+
+  stroke(50);
+  fill("blue");
+  circle(120, y, 60, 30);
+
+  stroke(50);
+  fill("yellow");
+  circle(150, y, 60, 30);
+
+  stroke(50);
+  fill("black");
+  circle(180, y, 60, 30);
+
+  stroke(50);
+  fill("green");
+  circle(210, y, 60, 30);
+
+  stroke(50);
+  fill("white");
+  circle(240, y, 60, 60);
+
+  stroke(50);
+  fill("brown");
+  circle(270, y, 60, 30);
+
+  stroke(50);
+  fill("purple");
+  circle(300, y, 60, 30);
+
+
+
+  
+  x = x + 1;
+ 
+  y = y - 1;
+  
+  if (y < 0) {
+    y = height;
+  }
 }
